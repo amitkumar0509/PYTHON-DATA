1fbde2d55d006220e50b8ea450606ce6a200da5b
amitkumar0509 <kumaramit01977@gmail.com>
2025-01-23 18:55:42 +0530

    `Removed binary cache file and deleted csv.py, no changes in actual code.`

---
+++ DSA/list.py
@@ -0,0 +1,16 @@
+list = ["amit","is","coder"]
+print("\nlist containing multiple values ")
+print(list)
+
+list2 = [["hello","my name"],["amit"]]
+print("\n multiple dimensional list ")
+print(list2)
+print(list[0])
+print(list2[0][1])
+print("Accessing element using negative indexing")
+    
+# print the last element of list
+print(list[-1])
+    
+# print the third last element of list
+print(list[-3])
---
+++ pandas/csv.py
@@ -0,0 +1,3 @@
+import pandas as pd 
+df = pd.read_csv("C:\\Users\\kumar\\people_data.csv")
+print(df)
