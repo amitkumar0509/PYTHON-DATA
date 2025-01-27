cf9276c9eb58e829e1a54421ad6ccb3c889bbfa2
amitkumar0509 <kumaramit01977@gmail.com>
2025-01-27 21:38:59 +0530

    git commit

---
+++ NUMPY/multiplication.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Element-wise addition
+x = np.array([1, 2, 3])
+y = np.array([4, 5, 6])
+print(x + y)  # Output: [5 7 9]
+
+# Matrix multiplication
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+print(np.dot(a, b))
