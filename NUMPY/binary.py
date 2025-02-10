dbae09bc50346d497fe6377e8eeeda8f0051d9e2
amitkumar0509 <kumaramit01977@gmail.com>
2025-02-10 21:38:29 +0530

    git commit

---
+++ NUMPY/binary.py
@@ -0,0 +1,45 @@
+# And(Product)
+
+import numpy as np
+arr = 10
+arr1=12
+c = np.bitwise_and(arr,arr1)
+print(c)
+
+# Python program explaining
+# bitwise_and() function
+
+import numpy as geek
+
+in_arr1 = [2, 8, 125]
+in_arr2 = [3, 3, 115]
+ 
+print ("Input array1 : ", in_arr1) 
+print ("Input array2 : ", in_arr2)
+  
+out_arr = geek.bitwise_and(in_arr1, in_arr2) 
+print ("Output array after bitwise_and: ", out_arr) 
+
+
+# or(Addition)
+import numpy as np
+arr = 10
+arr1=12
+c = np.bitwise_or(arr,arr1)
+print(c)
+
+# XOR
+# Python program explaining
+# bitwise_xor() function
+
+import numpy as geek
+in_num1 = 10
+in_num2 = 11
+
+print ("Input  number1 : ", in_num1)
+print ("Input  number2 : ", in_num2) 
+  
+out_num = geek.bitwise_xor(in_num1, in_num2) 
+print ("bitwise_xor of 10 and 11 : ", out_num) 
+
+
---
+++ NUMPY/leftside.py
@@ -0,0 +1,16 @@
+import numpy as np
+arr = 10
+shift = 3
+result = np.left_shift(arr,shift)
+print(result)
+
+
+import numpy as geek
+in_num = 20
+bit_shift = 2
+
+print ("Input  number : ", in_num)
+print ("Number of bit shift : ", bit_shift ) 
+  
+out_num = geek.right_shift(in_num, bit_shift) 
+print ("After right shifting 2 bit  : ", out_num) 
